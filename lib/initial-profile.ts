3920d6e82a8437574ffa5c4349d980eb8382c313
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-01-18 23:20:38 +0530

    add: initial profile

---
+++ lib/initial-profile.ts
@@ -0,0 +1,29 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import db from '@/lib/db'
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user)
+    return redirectToSignIn();
+
+  //* checking for existing profile and return it 
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    }
+  });
+  if (profile) return profile;  
+
+  //* create new profile in case no profile exist with such id 
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}` || "no name",
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress || " no email"
+    }
+  });
+  return newProfile;
+}
