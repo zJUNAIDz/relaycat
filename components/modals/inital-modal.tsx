e4cbcecf4d80d2fa55fa6f299abe710065653598
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-01-28 13:12:10 +0530

    integrate zod with useForm hook

--- components/modals/inital-modal.tsx
+++ components/modals/inital-modal.tsx
@@ -1,12 +1,60 @@
 "use client";
+import * as z from "zod";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "../ui/dialog";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required.",
+  }),
+  //TODO: remove this requirement and use fallback image if not specified
+  imageUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
+});
 
 const InitialModal = () => {
-  return <Dialog open>
-    <DialogTrigger>Create</DialogTrigger>
-    <DialogContent  className="bg-white text-black overflow-hidden">YOOO</DialogContent>
-  </Dialog>;
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  //* workaround to avoid Hydration warnings
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => setIsMounted(true), []);
+  if (!isMounted) return null;
+
+  const isLoading = form.formState.isLoading;
+
+  const onSubmit = (values: z.infer<typeof formSchema> ) => {};
+
+  return (
+    <Dialog open>
+      <DialogTrigger>Create</DialogTrigger>
+      <DialogContent className="bg-white text-black overflow-hidden">
+        <DialogHeader>
+          <DialogTitle className="text-center text-2xl font-bold">
+            Customize your Server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your Server a personality with a Name and an Image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default InitialModal;
