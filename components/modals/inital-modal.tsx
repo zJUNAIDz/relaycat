ff952e203fa8d3343d480ded86ecf529152b8163
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-08-14 23:42:34 +0530

    add initial modal

--- components/modals/inital-modal.tsx
+++ components/modals/inital-modal.tsx
@@ -1,15 +1,19 @@
 "use client";
-import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "../ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTrigger,
 } from "../ui/dialog";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -31,14 +35,14 @@ const InitialModal = () => {
   });
 
   //* workaround to avoid Hydration warnings
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => setIsMounted(true), []);
+  const [isMounted, setIsMounted] = React.useState(false);
+  React.useEffect(() => setIsMounted(true), []);
   if (!isMounted) return null;
 
   const isLoading = form.formState.isLoading;
 
-  const onSubmit = (values: z.infer<typeof formSchema> ) => {};
-
+  const onSubmit = (values: z.infer<typeof formSchema>) => { };
+  
   return (
     <Dialog open>
       <DialogTrigger>Create</DialogTrigger>
@@ -52,8 +56,45 @@ const InitialModal = () => {
             always change it later.
           </DialogDescription>
         </DialogHeader>
-      </DialogContent>
-    </Dialog>
+        <FormProvider {...form} >
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex justify-center items-center text-center">
+                TODO: Image Upload
+              </div>
+
+              <div>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel
+                        className="uppercase text-xs font-bold text-zinc-700 dark:text-secondary/70"
+                      >
+                        Server Name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visble:ring-offset-0"
+                          placeholder="Enter Server Name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>Create</Button>
+            </DialogFooter>
+          </form>
+        </FormProvider>
+      </DialogContent >
+    </Dialog >
   );
 };
 
