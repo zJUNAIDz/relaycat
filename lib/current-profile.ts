f16e73863834e9df2975e46eef4d37b11a233002
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-08-18 12:56:52 +0530

    add utility function to get current profile

---
+++ lib/current-profile.ts
@@ -0,0 +1,14 @@
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/prisma"
+
+const currentProfile = async () => {
+  const { userId } = auth();
+  if (!userId) return null;
+  const profile = await db.profile.findUnique({
+    where: {
+      userId
+    }
+  });
+  return profile;
+}
+export default currentProfile;
