011c5c8202563892044305c58744c69d0f4e6f92
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-08-21 23:43:49 +0530

    add layout for main routes

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
+
+const MainLayout: React.FC<{ children: React.ReactNode }> = async ({ children }) => {
+  return <div className="h-full">
+    <aside className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+      <NavigationSidebar />
+    </aside>
+    <main className="md:pl-[72px] h-full">
+      {children}
+    </main>
+  </div>
+}
+
+export default MainLayout;
