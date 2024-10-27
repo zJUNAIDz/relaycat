6a44d8729ae6355f541bc9d957049f82f2c8deef
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-10-28 01:02:48 +0530

    remove clerk user button component

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -6,51 +6,42 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
 import NavigationItem from "./navigation-item";
-import { UserButton } from "@clerk/nextjs";
 
 const NavigationSidebar = async () => {
-
-  const profile = await currentProfile();
-  if (!profile) return redirect("/");
+  const user = await currentProfile();
+  if (!user) return redirect("/");
 
   const servers = await db.server.findMany({
     where: {
       members: {
         some: {
-          profileId: profile.id,
-        }
-      }
-    }
+          userId: user.id,
+        },
+      },
+    },
   });
 
-  return <div
-    className="space-y-4 flex flex-col h-full items-center text-primary w-full
+  return (
+    <div
+      className="space-y-4 flex flex-col h-full items-center text-primary w-full
     dark:bg-[#1e1f22] bg-[#e3e5e8] py-3"
-  >
-    <NavigationAction />
-    <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
-    <ScrollArea className="flex flex-1 w-full justify-center items-center ">
-      {servers.map(({ id, name, imageUrl }) => (
-        <div key={id} className="mb-4 flex w-full justify-center items-center">
-          <NavigationItem
-            id={id}
-            name={name}
-            imageUrl={imageUrl}
-          />
-        </div>
-      ))}
-    </ScrollArea>
-    <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
-      <ModeToggle />
-      <UserButton
-        afterSignOutUrl="/"
-        appearance={{
-          elements: {
-            avatarBox: "h-[48px] w-[48px]"
-          }
-        }}
-      />
+    >
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex flex-1 w-full justify-center items-center ">
+        {servers.map(({ id, name, image }) => (
+          <div
+            key={id}
+            className="mb-4 flex w-full justify-center items-center"
+          >
+            <NavigationItem id={id} name={name} imageUrl={image} />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+      </div>
     </div>
-  </div>
-}
+  );
+};
 export default NavigationSidebar;
