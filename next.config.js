3475d59bf276fc9cd317399f5d3e79a0b5b38017
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-10-30 01:22:21 +0530

    install aws-sdk and add s3 url to next image config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "utfs.io",
-      pathname: "/**",
-    }]
-  }
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "global.discourse-cdn.com",
+        pathname:
+          "/turtlehead/original/2X/c/c830d1dee245de3c851f0f88b6c57c83c69f3ace.png",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
