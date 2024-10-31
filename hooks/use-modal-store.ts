cc48e75e639c6f9a490f1290abfe136b1e5ae607
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-10-31 22:41:22 +0530

    update useModal hook to manage modalData(server)

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,19 +1,27 @@
+import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "editServer" | "createChannel";
+export type ModalType =
+  | "createServer"
+  | "editServer"
+  | "createChannel"
+  | "invite";
 
+interface ModalData {
+  server?: Server;
+}
 interface ModalStore {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType) => void;
+  onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
+  data: {},
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type }),
-  onClose: () => set({ isOpen: false })
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ isOpen: false }),
 }));
-
-
