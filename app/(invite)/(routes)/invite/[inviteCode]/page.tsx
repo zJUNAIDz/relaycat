c14610b2f6cef43b303f58c94cf9254d754ffbcd
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-11-01 12:50:34 +0530

    add server invite page

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,50 @@
+import { auth } from "@/auth";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface InviteCodePageProps {
+  params: {
+    inviteCode: string;
+  };
+}
+
+const InviteCodePage: React.FC<InviteCodePageProps> = async ({
+  params: { inviteCode },
+}) => {
+  const profile = await auth().then((session) => session?.user);
+  if (!profile) return redirect("/login");
+
+  //* If user already in invited server, it will be redirected to that server
+  const alreadyInServer = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          userId: profile.id,
+        },
+      },
+      inviteCode: inviteCode,
+    },
+  });
+  if (alreadyInServer) return redirect(`/servers/${alreadyInServer.id}`);
+
+  //* if not already in server then add it
+  const server = await db.server.update({
+    where: {
+      inviteCode: inviteCode,
+    },
+    data: {
+      members: {
+        create: {
+          userId: profile.id,
+        },
+      },
+    },
+  });
+  if (server) return redirect(`/servers/${server.id}`);
+
+  //TODO: Add UI to accept or reject invite
+  return <div>Invite link is Broken</div>;
+};
+
+export default InviteCodePage;
