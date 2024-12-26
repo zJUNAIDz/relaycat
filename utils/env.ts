57d96767e806eea7aef603bc4c0ada676c1bf6a1
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-12-26 22:11:31 +0530

    improvise env handling logic

---
+++ utils/env.ts
@@ -0,0 +1,9 @@
+import "server-only"
+
+export const getEnv = (key: string): string => {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+  return value;
+}
