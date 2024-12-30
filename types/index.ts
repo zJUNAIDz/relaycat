1b5b46f19d95bab379847f9fdc517b41216b5524
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-12-30 21:19:21 +0530

    add more types

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,37 @@
 import { Server, Member, User } from "@prisma/client";
+import exp from "constants";
 
 export type ServerWithMembersWithUserProfiles = Server & {
   members: (Member & { user: User })[];
 };
+
+export const UserAuthStatus = {
+  AUTHENTICATED: "AUTHENTICATED",
+  UNAUTHENTICATED: "UNAUTHENTICATED",
+  NOTFOUND: "NOTFOUND",
+  // BANNED: "BANNED", //TODO: Make it applicable
+  DELETED: "DELETED",
+  ERROR: "ERROR",
+} as const;
+export type UserAuthStatusType = keyof typeof UserAuthStatus;
+
+export type UserProfileResponse = {
+  profile: User | null;
+  status: UserAuthStatusType;
+}
+
+
+export interface CreateServerRequest {
+  name: string;
+  imageUrl: string;
+}
+export const CreateServerResponseStatus = {
+  SUCCESS: "SUCCESS",
+  FAILED: "FAILED",
+  ERROR: "ERROR",
+} as const;
+export type CreateServerResponseStatusType = keyof typeof CreateServerResponseStatus;
+export interface CreateServerResponse {
+  server: Server | null;
+  status: CreateServerResponseStatusType;
+}
