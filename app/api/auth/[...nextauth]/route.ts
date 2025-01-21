a259a3349089ee83dca32f1b85f862973aee57b8
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-01-21 22:47:10 +0530

    chores: formatting

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,3 @@
-import { GET, POST } from "@/auth" // Referring to the auth.ts we just created
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+const { GET, POST } = handlers
 export { GET, POST }
