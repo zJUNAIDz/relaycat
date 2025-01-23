8530a632ee124c3ace1ef30526de08ffd62597c1
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-01-23 14:13:14 +0530

    implement `ServerSearch` component and add it to server sidebar

--- features/server/components/server-sidebar.tsx
+++ features/server/components/server-sidebar.tsx
@@ -1,12 +1,29 @@
 import { auth } from "@/auth";
 import { db } from "@/shared/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
 import ServerHeader from "./server-header";
+import { ScrollArea } from "@/shared/components/ui/scroll-area";
+import { ServerSearch } from "@/features/server/navigation/server-search";
+import { Hash, Mic, Video } from "lucide-react";
+import { RoleIcon } from "@/shared/components/icons";
 interface ServerSidebarProps {
   serverId: string;
 }
+
+const channelIconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.ADMIN]: <RoleIcon role={MemberRole.ADMIN} />,
+  [MemberRole.MODERATOR]: <RoleIcon role={MemberRole.MODERATOR} />,
+  [MemberRole.GUEST]: <RoleIcon role={MemberRole.GUEST} />,
+}
+
 const ServerSidebar: React.FC<ServerSidebarProps> = async ({ serverId }) => {
   const user = await auth().then((session) => session?.user);
   if (!user) return redirect("/login");
@@ -48,7 +65,7 @@ const ServerSidebar: React.FC<ServerSidebarProps> = async ({ serverId }) => {
   const videoChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.VIDEO
   );
-  const members = server?.members.filter((member) => member.userId !== user.id);
+  const members = server?.members;
   const role = server?.members.find(
     (member) => member.userId === user.id
   )?.role;
@@ -56,6 +73,48 @@ const ServerSidebar: React.FC<ServerSidebarProps> = async ({ serverId }) => {
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <ServerSearch
+          data={[
+            {
+              label: "Text Channels",
+              type: "channel",
+              data: textChannels.map(channel => ({
+                icon: channelIconMap[channel.type],
+                name: channel.name,
+                id: channel.id
+              }))
+            },
+            {
+              label: "Voice Channels",
+              type: "channel",
+              data: audioChannels.map(channel => ({
+                icon: channelIconMap[channel.type],
+                name: channel.name,
+                id: channel.id
+              }))
+            },
+            {
+              label: "Video Channels",
+              type: "channel",
+              data: videoChannels.map(channel => ({
+                icon: channelIconMap[channel.type],
+                name: channel.name,
+                id: channel.id
+              }))
+            },
+            {
+              label: "Members",
+              type: "member",
+              data: members?.map(member => ({
+                icon: roleIconMap[member.role],
+                name: member.user.name || "",
+                id: member.userId
+              }))
+            },
+          ]}
+        />
+      </ScrollArea>
     </div>
   );
 };
---
+++ features/server/navigation/server-search.tsx
@@ -0,0 +1,95 @@
+"use client"
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/shared/components/ui/command";
+import { Dialog, DialogHeader, DialogTitle } from "@/shared/components/ui/dialog";
+import { Router, Search } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+interface ServerSearchProps {
+  data: {
+    label: string;
+    type: "member" | "channel";
+    data: {
+      icon: React.ReactNode;
+      name: string;
+      id: string;
+    }[] | undefined
+  }[]
+}
+const cmdKey = navigator.userAgent.toUpperCase().indexOf("MAC") >= 0 ? "&#8984;" : "CTRL";
+export const ServerSearch: React.FC<ServerSearchProps> = ({ data }) => {
+  const router = useRouter()
+  const params = useParams()
+  useEffect(() => {
+    const onKeyPress = (e: KeyboardEvent) => {
+      if (e.key === "s" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    }
+    document.addEventListener("keydown", onKeyPress);
+    return () => document.removeEventListener("keydown", onKeyPress);
+  }, [])
+
+  const [open, setOpen] = React.useState(false)
+
+  const onClick = ({ id, type }: { id: string; type: "channel" | "member" }) => {
+    setOpen(false)
+    if (type === "member") {
+      router.push(`/servers/${params.serverId}/conversations/${id}`)
+      return
+    }
+    if (type === "channel") {
+      router.push(`/servers/${params.serverId}/channels/${id}`)
+      return
+    }
+  }
+
+  return (
+    <>
+      <button onClick={() => setOpen(true)} className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-200 dark:hover:bg-zinc-700/50 transition">
+        <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
+        <p
+          className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 trnsition"
+        >
+          Search
+        </p>
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted  px-1.5  font-mono text-[10px] font-medium  text-muted-foreground ml-auto">
+          <span className="text-xs">{cmdKey}</span> K
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <DialogHeader>
+          <DialogTitle className="text-center sr-only">
+            Search all channels and members
+          </DialogTitle>
+          <CommandInput placeholder="search all channels and members" />
+        </DialogHeader>
+        <CommandList>
+          <CommandEmpty>
+            No Results Found
+          </CommandEmpty>
+          {
+            data.map(({ label, type, data }) => {
+              if (!data) return null
+              return (
+                <CommandGroup key={label} heading={label}>
+                  {
+                    data?.map(({ id, icon, name }) => (
+                      <CommandItem key={id} onSelect={() => onClick({ id, type })}>
+                        <div className="flex items-center gap-x-2">
+                          {icon}
+                          {name}
+                        </div>
+                      </CommandItem>
+                    ))
+                  }
+                </CommandGroup>
+              )
+            })
+          }
+        </CommandList>
+      </CommandDialog >
+    </>
+  )
+}
