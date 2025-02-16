400d4091c66389ab072606bf56e31e889f9a107a
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-02-16 21:41:39 +0530

    add: Icons component

--- shared/components/icons.tsx
+++ shared/components/icons.tsx
@@ -21,3 +21,26 @@ export const RoleIcon: React.FC<RoleIconProps> = ({ role, className }) => (
     width={20}
   />
 );
+
+interface ChannelIconProps {
+  type: string;
+  className?: string;
+  height?: number;
+  width?: number;
+}
+const channelIconMap: Record<string, string> = {
+  "TEXT": "/icons/text.png",
+  "AUDIO": "/icons/audio.png",
+  "VIDEO": "/icons/video.png",
+};
+
+export const ChannelIcon: React.FC<ChannelIconProps> = ({ type, className, height, width }) => (
+  <Image
+    className={className || ""}
+    src={channelIconMap[type]}
+    alt={type}
+    height={height || 20}
+    width={width || 20}
+  />
+
+)
