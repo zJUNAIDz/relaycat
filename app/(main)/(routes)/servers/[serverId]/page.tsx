bea6df3a33b77a2b6a1694a6673060e67d1edc32
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-02-19 21:37:14 +0530

    feat✨: redirects to first channel of server clicking on server from navigation bar will redirect user to its first channel or else show default server page if not channel exist

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,39 @@
+import DefaultServerPage from "@/features/server/components/default-server-page";
+import currentProfile from "@/shared/lib/current-profile";
+import { db } from "@/shared/lib/db";
+import { redirect } from "next/navigation";
+import React from "react";
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+const ServerIdPage: React.FC<ServerIdPageProps> = async ({ params: { serverId } }) => {
 
-const ServerIdPage = () => {
-  return <div>
-    <h1>Server Page</h1>
-  </div >
+  const { profile } = await currentProfile();
+  const channel = await db.channel.findFirst({
+    where: {
+      server: {
+        id: serverId,
+        members: {
+          some: {
+            user: {
+              id: profile?.id
+            }
+          }
+        }
+      },
+    },
+    orderBy: {
+      createdAt: "asc"
+    },
+    select: {
+      id: true,
+    }
+  })
+  if (!channel) {
+    return <DefaultServerPage />;
+  }
+  return redirect(`/servers/${serverId}/channels/${channel.id}`)
 }
 export default ServerIdPage;
