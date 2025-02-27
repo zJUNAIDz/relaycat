a931d07b990efd3662aa6eaae51b41afed83a60d
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-02-27 22:18:38 +0530

    perf👌: add prisma optimize layer for db performance insights

--- shared/lib/db.ts
+++ shared/lib/db.ts
@@ -1,8 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-
+import { withOptimize } from '@prisma/extension-optimize'
+import { getEnv } from '../utils/env';
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const db =
-  globalForPrisma.prisma || new PrismaClient()
+  globalForPrisma.prisma || new PrismaClient().$extends(withOptimize({ apiKey: getEnv("OPTIMIZE_API_KEY"), enable: true }));
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
