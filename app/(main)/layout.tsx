cecd0773697ca0f735f1385f7ae137ff5dcee1eb
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-02-27 22:05:17 +0530

    fix🐛: hide navigation-sidebar for mobile devices

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import NavigationSidebar from "@/features/server/navigation/navigation-sidebar";
 const MainLayout: React.FC<{ children: React.ReactNode }> = async ({ children }) => {
 
   return <div className="h-full">
-    <aside className=" md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+    <aside className="hidden md:flex h-full w-0 md:w-[72px] z-30 flex-col fixed inset-y-0">
       <NavigationSidebar />
     </aside>
     <main className="md:pl-[72px] h-full">
