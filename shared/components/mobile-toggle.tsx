0b1cb0ed8cf9c712c6da6f1cac239cd6f63076fc
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-02-27 22:13:06 +0530

    feat✨: implement mobile-toggle component to toggle between navigation bar and chat page server navigation and sidebar will be hidden for mobile devices and but can be accessed using toggle menu

---
+++ shared/components/mobile-toggle.tsx
@@ -0,0 +1,24 @@
+import { Menu } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import { Button } from "./ui/button"
+import NavigationSidebar from "@/features/server/navigation/navigation-sidebar"
+import ServerSidebar from "@/features/server/components/server-sidebar"
+import { Server } from "@prisma/client"
+
+export const MobileToggle = ({ serverId }: { serverId: Server["id"] }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  )
+}
