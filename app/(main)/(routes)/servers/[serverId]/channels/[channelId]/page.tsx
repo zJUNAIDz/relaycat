ac0a5e88009d703598f58f3d9df56ce027ce3647
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-03-01 04:35:24 +0530

    fix🐛: fix authorization header there was extra `})` at the end of token template literal which led to hours of debugging for no reason

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,53 +1,50 @@
 import ChatHeader from "@/features/chat/components/chat-header";
+import { API_URL } from "@/shared/lib/constants";
 import currentProfile from "@/shared/lib/current-profile";
-import { db } from "@/shared/lib/db";
+import { getAuthTokenOnServer, getCurrentUser } from "@/shared/utils/server";
+import axios from "axios";
 import { redirect } from "next/navigation";
 interface ChannelIdPageProps {
   params: any
 }
 
-export async function generateMetadata({ params }: ChannelIdPageProps) {
-  const { channelId } = await params
-  const channel = await db.channel.findUnique({
-    where: {
-      id: channelId,
-    },
-    include: {
-      server: {
-        select: {
-          name: true,
-        }
-      }
-    }
-  })
 
-  return {
-    title: `#${channel?.name} | ${channel?.server.name} `,
-  }
-}
+// export async function generateMetadata({ params }: ChannelIdPageProps) {
+//   const { channelId } = await params
+//   // const channel = await db.channel.findUnique({
+//   //   where: {
+//   //     id: channelId,
+//   //   },
+//   //   include: {
+//   //     server: {
+//   //       select: {
+//   //         name: true,
+//   //       }
+//   //     }
+//   //   }
+//   // })
+
+//   return {
+//     title: "channel"
+//     // title: `#${channel?.name} | ${channel?.server.name} `,
+//   }
+// }
 
 
 const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   const { serverId, channelId } = await params
-  const { profile } = await currentProfile();
-  if (!profile) {
+  const user = getCurrentUser();
+  if (!user) {
     return redirect("/auth")
   }
   //*TODO: move db logic to server
-  const channel = await db.channel.findUnique({
-    where: {
-      id: channelId,
+  const url = `${API_URL}/channels/${channelId}`
+  const { data: { channel } } = await axios.get(url, {
+    headers: {
+      "Authorization": `Bearer ${await getAuthTokenOnServer()}`
     }
   })
-  const member = await db.member.findFirst({
-    where: {
-      serverId,
-      user: {
-        id: profile.id
-      }
-    }
-  })
-  if (!channel || !member) {
+  if (!channel) {
     redirect("/")
   }
   return <div className="bg-white dark:bg-[#313338]">
