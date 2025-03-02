8639d4f2882874d08949d809c5d1075d436434be
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-03-02 19:39:18 +0530

    patch🚑: update metadata

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,9 @@
 import { getCurrentUser } from "@/shared/utils/server"
 import Landing from "./landing"
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "RelayCat | Fast. Connected. Inspired."
+}
 const LandingPage = async () => {
   const user = await getCurrentUser();
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
+import { ModalProvider } from "@/shared/providers/modal-provider";
 import { ThemeProvider } from "@/shared/providers/theme-provider";
 import { cn } from "@/shared/utils/cn";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { ModalProvider } from "@/shared/providers/modal-provider";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "RelayCat",
-  description: "A Communication Platform",
+  description: "A Fast & Secure Communication Platform",
+
 };
 
 export default function RootLayout({
