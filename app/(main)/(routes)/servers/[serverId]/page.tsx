deaf1cd4894fdb6c174cd37c8e4a6c852b2dbfc0
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-03-02 20:26:48 +0530

    refactor🎨: move db logic to server

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,9 @@
 import DefaultServerPage from "@/features/server/components/default-server-page";
-import currentProfile from "@/shared/lib/current-profile";
-import { db } from "@/shared/lib/db";
+import { API_URL } from "@/shared/lib/constants";
+import { getAuthTokenOnServer, getCurrentUser } from "@/shared/utils/server";
+import axios from "axios";
 import { redirect } from "next/navigation";
+import queryString from "query-string";
 interface ServerIdPageProps {
   params: Promise<{
     serverId: string;
@@ -11,30 +13,24 @@ interface ServerIdPageProps {
 
 const ServerIdPage = async ({ params }: ServerIdPageProps) => {
   const { serverId } = await params
-  const { profile } = await currentProfile();
-  const channel = await db.channel.findFirst({
-    where: {
-      server: {
-        id: serverId,
-        members: {
-          some: {
-            user: {
-              id: profile?.id
-            }
-          }
-        }
-      },
-    },
-    orderBy: {
-      createdAt: "asc"
-    },
-    select: {
-      id: true,
+  const user = await getCurrentUser()
+  if (!user) {
+    redirect("/auth?login=")
+  }
+  const url = queryString.stringifyUrl({
+    url: `${API_URL}/channels`,
+    query: {
+      serverId,
     }
   })
-  if (!channel) {
+  const { data: { channels } } = await axios.get(url, {
+    headers: {
+      "Authorization": `Bearer ${await getAuthTokenOnServer()}`
+    }
+  })
+  if (!channels) {
     return <DefaultServerPage />;
   }
-  return redirect(`/servers/${serverId}/channels/${channel.id}`)
+  return redirect(`/servers/${serverId}/channels/${channels[0].id}`)
 }
 export default ServerIdPage;
