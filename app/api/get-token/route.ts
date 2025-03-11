5a532f6168f0fa17bc3268d99d35fd3670d1cb31
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-03-12 02:06:55 +0530

    patch🚑: update `get-token` api route to return user object as well

--- app/api/get-token/route.ts
+++ app/api/get-token/route.ts
@@ -3,12 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
+
     const session = await auth();
     const token = session?.apiToken;
     if (!token) {
       return NextResponse.json({ error: "No token found", success: false });
     }
-    return NextResponse.json({ token, success: true });
+    const user = session?.user;
+    return NextResponse.json({ token, user, success: true });
   } catch (e) {
     return NextResponse.json({ error: e, success: false });
   }
