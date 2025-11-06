9bdc5f8b46cf485b5cb9f8ad87daec23ee0e1a7b
Junaid Shaikh <junaidshaikh3967@gmail.com>
2025-11-06 17:23:47 +0530

    add prisma config

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import * as path from "node:path";
+import type { PrismaConfig } from "prisma";
+
+export default {
+  schema: path.join("./prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("./prisma", "migrations"),
+  },
+  views: {
+    path: path.join("./prisma", "views"),
+  },
+  typedSql: {
+    path: path.join("./prisma", "queries"),
+  }
+} satisfies PrismaConfig;
