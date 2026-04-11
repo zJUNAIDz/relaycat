ff9d7bf8150aaecfd0215ebf4cc572f9369ec065
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-04-11 17:52:12 +0530

    refactor(server): add SSR compatibility by adding `headers()` to fetchOptions

--- apps/web/shared/utils/server.ts
+++ apps/web/shared/utils/server.ts
@@ -1,15 +1,20 @@
 import { authClient } from "@/shared/lib/auth-client";
+import { headers } from "next/headers";
 
 export const getCurrentUser = async () => {
   try {
-    const { data, error } = await authClient.getSession();
-    console.log({ shouBeError: data });
+    const { data, error } = await authClient.getSession({
+      fetchOptions: {
+        headers: await headers(),
+      },
+    });
+    // console.log({ data, error });
     if (error) return null;
     return data?.user || null;
   } catch (err) {
     console.warn(
       "getCurrentUser: authClient.getSession() failed during build/prerender",
-      err
+      err,
     );
     return null;
   }
