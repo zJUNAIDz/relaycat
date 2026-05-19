0f50777cd5e83ede84386b20757d92c0d3393ad0
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-05-19 20:42:06 +0530

    rename(serverId layout): move sidebar layout to serverId from channelId

--- apps/web/app/(main)/(routes)/channels/[serverId]/layout.tsx
+++ apps/web/app/(main)/(routes)/channels/[serverId]/layout.tsx
@@ -4,18 +4,21 @@ interface ServerIdLayoutProps {
   children: React.ReactNode;
   params: Promise<{
     serverId: string;
+    channelId: string;
   }>
 }
 const ServerIdLayout: React.FC<ServerIdLayoutProps> = async ({
   children,
   params,
 }) => {
+  const { serverId, channelId } = await params;
+  console.log("ServerIdLayout params: ", { serverId, channelId });
   return (
     <div className="h-full">
-      <div className="hidden h-full w-60 z-20 fixed inset-y-0">
-        <ServerSidebar />
+      <div className="hidden md:flex h-full w-60 z-20 fixed inset-y-0">
+        <ServerSidebar serverId={serverId} channelId={channelId} />
       </div>
-      <main className="h-full md:pl-60">{children} </main>
+      <main className="h-full md:pl-60">{children}</main>
     </div>
   );
 };
