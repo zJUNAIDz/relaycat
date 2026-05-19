d8e573d93107c20aa1f70391c9d9261dc3b0d339
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-05-19 07:43:55 +0530

    refactor(mobile-toggle): simplify MobileToggle component by removing unused context and improving structure

--- apps/web/shared/components/mobile-toggle.tsx
+++ apps/web/shared/components/mobile-toggle.tsx
@@ -1,34 +1,23 @@
-"use client";
-import ServerSidebar from "@/features/server/components/server-sidebar"
-import NavigationSidebar from "@/features/server/navigation/navigation-sidebar"
-import { Menu } from "lucide-react"
-import { Button } from "./ui/button"
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
+import ServerSidebar from "@/features/server/components/server-sidebar";
+import NavigationSidebar from "@/features/server/navigation/navigation-sidebar";
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 export const MobileToggle = () => {
-  const ctx = useAppContextStore()  
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="ghost" size="icon" className="md:hidden">
+        <Button variant="ghost" size="icon" className="flex md:hidden">
           <Menu className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="p-0 flex gap-0">
+      <SheetContent side="left" className="p-0 flex gap-0" aria-description="mobile sidebar">
+        <SheetTitle className="sr-only">Mobile Navigation</SheetTitle>
         <div className="w-18">
           <NavigationSidebar />
         </div>
-        {
-          ctx.currentServerId ? (
-            <ServerSidebar serverId={ctx.currentServerId} />
-          ) : (
-            <div className="flex-1 flex items-center justify-center">
-              <p className="text-zinc-500 dark:text-zinc-400">
-                Select a server to view its channels
-              </p>
-            </div>
-          )
-        }
+        <ServerSidebar />
       </SheetContent>
     </Sheet>
   )
