e28dab48e6e20733d8122d8093fb4aae8c715803
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-05-29 07:51:51 +0530

    refactor: streamline chat query hook by improving URL construction and removing unused imports

--- apps/web/features/chat/hooks/chat-query-hook.ts
+++ apps/web/features/chat/hooks/chat-query-hook.ts
@@ -1,9 +1,21 @@
 import axiosClient from "@/shared/lib/axios-client";
 import { useAuth } from "@/shared/providers/auth-provider";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import axios from "axios";
-import qs from "query-string";
+import queryString from "query-string";
+import { z } from "zod/v3";
 
+export const cursorSchema = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("before"),
+    limit: z.coerce.number().min(1).max(100),
+    before: z.string(),
+  }),
+  z.object({
+    type: z.literal("after"),
+    limit: z.coerce.number().min(1).max(100),
+    after: z.string(),
+  }),
+]);
 interface ChatQueryProps {
   queryKey: string;
   apiUrl: string;
@@ -15,47 +27,36 @@ export const useChatQuery = ({
   queryKey,
   apiUrl,
   paramKey,
-  paramValue
+  paramValue,
 }: ChatQueryProps) => {
   const { isLoading, session } = useAuth();
 
   const fetchMessages = async ({ pageParam = undefined }) => {
     // Add runtime validation (safety net)
-
-
-    const url = qs.stringifyUrl({
+    const url = queryString.stringifyUrl({
       url: apiUrl,
-      query: {
-        cursor: pageParam,
-        [paramKey]: paramValue
-      }
-    }, { skipNull: true });
-
+      query: pageParam,
+    });
     const { data } = await axiosClient.get(url);
 
-    return data;
+    return data.result;
   };
 
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    status
-  } = useInfiniteQuery({
-    queryKey: [queryKey], // Include paramValue in query key
-    queryFn: fetchMessages,
-    initialPageParam: undefined,
-    getNextPageParam: (lastPage) => lastPage.nextCursor,
-    refetchInterval: false,
-    enabled: !isLoading && !!session// Only enable when token is available
-  });
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: [queryKey], // Include paramValue in query key
+      queryFn: fetchMessages,
+      initialPageParam: undefined,
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      refetchInterval: false,
+      enabled: !isLoading && !!session, // Only enable when token is available
+    });
 
   return {
     data,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
-    status
+    status,
   };
 };
